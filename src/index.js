8ec7d197db01f31e6ad753936c5a0734351aa8ee
ricomen <ricomen@mail.ru>
2020-07-12 21:45:08 +0500

    Получение данных обработка ошибок

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('red')
+const getResource = async ( url ) => {
+    const res = await fetch( url );
+
+    if( !res.ok ) {
+
+        throw  new Error(`Could not fetch ${url}, reciver ${res.status}`)
+
+    };
+
+    const body = await res.json();
+
+    return body;
+};
+
+getResource('https://swapi.dev/api/people/1ldfmvs/')
+    .then(( body ) => {
+        console.log( body );
+    })
+    .catch((err) => {
+        console.log( err );
+    });
