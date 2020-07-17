7ac2fc360cb5b4ad7f86348779cb17abd2121496
ricomen <ricomen@mail.ru>
2020-07-17 21:40:42 +0500

    Added spinner

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import SwapiService from '../../services/swapi-service';
 
+import Spinner from "../spinner";
+
 import './random-planet.css';
 
 export default class RandomPlanet extends Component {
